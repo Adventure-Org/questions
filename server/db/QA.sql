c67d2fbeff60f62eb1ee9d6a55f7b967ba82f948
rochun <royce.h.chun@gmail.com>
2022-08-02 16:54:18 -0700

    refactor file path for EC2

--- server/db/QA.sql
+++ server/db/QA.sql
@@ -45,11 +45,11 @@ CREATE TABLE answer_photos (
 -- \COPY answer_photos(id, answer_id, url) FROM '/Users/roycechun/Desktop/RFP2205/Atelier-Backend/data/answers_photos.csv' CSV HEADER;
 
 
-\COPY questions(id , product_id, body, date_written, asker_name, asker_email, reported, helpful) FROM '../data/questions.csv' CSV HEADER;
+\COPY questions(id , product_id, body, date_written, asker_name, asker_email, reported, helpful) FROM '/home/unbuntu/data/questions.csv' CSV HEADER;
 
-\COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) FROM '../data/answers.csv' CSV HEADER;
+\COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) FROM '/home/ubuntu/data/answers.csv' CSV HEADER;
 
-\COPY answer_photos(id, answer_id, url) FROM '../data/answers_photos.csv' CSV HEADER;
+\COPY answer_photos(id, answer_id, url) FROM '/home/ubuntu/data/answers_photos.csv' CSV HEADER;
 
 ALTER TABLE questions
 ALTER COLUMN date_written TYPE timestamp
