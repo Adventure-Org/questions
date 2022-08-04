3a946c5639b56a035e204024da6c8ff8f997ff88
rochun <royce.h.chun@gmail.com>
2022-08-04 11:14:36 -0700

    input password into db pool

--- server/models/QA_Model.js
+++ server/models/QA_Model.js
@@ -6,6 +6,7 @@ const pool = new Pool({
   host: process.env.host,
   user: process.env.user,
   database: process.env.database,
+  password: process.env.password,
   port: process.env.dbport,
 });
 
