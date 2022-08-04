f554ac5ef5408f97a6cf951775b1c3c3771de07c
rochun <royce.h.chun@gmail.com>
2022-08-04 11:30:23 -0700

    add loader.io route

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use('/', router);
 
+app.get('/loaderio-3de1b3c04d31e9982aca7972e40f1698', (req, res) => {
+  res.send('loaderio-3de1b3c04d31e9982aca7972e40f1698');
+})
+
 const PORT = process.env.port || 3000;
 
 app.listen(PORT);
